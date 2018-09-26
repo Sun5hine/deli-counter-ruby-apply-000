9b62034949ad09fd569a23ef15ff9bc08c6c4945
sun5hine <af5kh@yahoo.com>
2018-09-26 14:45:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,4 +13,10 @@ katz_deli.push(name)
 puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def 
+def now_serving(katz_deli)
+  katz_deli.shift
+  if katz_deli.length == 0 
+    puts "There is no one to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}"
+  puts ""
